023a72d2f7d3e821433f1d2e77ac76d3d8db3344
Donna Peplinskie <donna.peplinskie@risevision.com>
2016-08-12 11:21:55 -0400

    Listen for requests at localhost:9494

--- test/test-server.js
+++ test/test-server.js
@@ -1,5 +1,16 @@
 var chai = require("chai"),
-  chaiHttp = require("chai-http"),
-  server = require("../server.js");
+  chaiHttp = require("chai-http");
 
 chai.use(chaiHttp);
+
+describe("Loading", function () {
+  var server;
+
+  beforeEach(function () {
+    server = require("../server.js");
+  });
+
+  afterEach(function () {
+    server.close();
+  });
+});
