916195ae70c6a9d16462a4d30e5260a825c97418
Donna Peplinskie <donna.peplinskie@risevision.com>
2016-10-13 16:56:02 -0400

    Respond to GET requests at /spreadsheets endpoint

--- app/routes/spreadsheets.js
+++ app/routes/spreadsheets.js
@@ -6,6 +6,33 @@ const Data = require("../models/data");
 const DataController = require("../controllers/data");
 
 const SpreadsheetsRoute = function(app, db) {
+  const model = new Data({}, db);
+  const controller = new DataController(model);
+
+  app.get("/spreadsheets", (req, res, next) => {
+    const key = req.query.key;
+
+    if (!key) {
+      res.statusCode = 400;
+
+      return next(new Error("Missing key parameter"));
+    }
+
+    controller.on("data", (data) => {
+      res.json(data);
+    });
+
+    controller.on("no-data", (data) => {
+      res.json({});
+    });
+
+    controller.on("data-error", (err) => {
+      console.error(err, key, new Date());
+      // TODO: Return response.
+    });
+
+    controller.getData(key);
+  });
 
   app.post("/spreadsheets", jsonParser, (req, res, next) => {
     if (!("key" in req.body) || !("value" in req.body)) {
@@ -13,9 +40,6 @@ const SpreadsheetsRoute = function(app, db) {
 
       return next(new Error("Missing POST data"));
     }
-
-    const model = new Data({}, db);
-    const controller = new DataController(model);
 
     controller.on("save-data", (data) => {
       res.status(201).json(data);
