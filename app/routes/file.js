a4b4e76ec3c5614ff46baefe7c95995c3cfdcf33
Francisco Vallarino <fjvallarino@gmail.com>
2017-06-15 20:13:38 -0300

    Refactor downloading code in order to reuse it. Properly update latest field when downloading a stale version.

--- app/routes/file.js
+++ app/routes/file.js
@@ -43,35 +43,11 @@ const FileRoute = function(app, headerDB, riseDisplayNetworkII, config, logger) 
               logger.info("File is not on latest version: " + fileUrl);
 
               // Check if the file is downloading.
-              fileSystem.isDownloading(fileUrl, (downloading) => {                
-                if (!downloading) {
-                  logger.info("File is not downloading: " + fileUrl);
-
-                  // get the appropriate header field for request
-                  controller.getUpdateHeaderField((err, field) => {
-                    if (err) { logger.error(err, null, fileUrl); }
-
-                    controller.on("headers-error", (err) => {
-                      logger.error("Could not save headers", err, fileUrl);
-                    });
-
-                    controller.on("downloaded", () => {
-                      logger.info("File downloaded", fileUrl);
-                    });
-
-                    controller.on("request-error", (err) => {
-                      logger.error(err, null, fileUrl);
-                    });
-
-                    controller.on("move-file-error", (err) => {
-                      logger.error(err, null, fileUrl);
-                    });
-
-                    // Make request to download file passing request header
-                    logger.info("File download started: " + fileUrl);
-                    controller.downloadFile(field);
-                  });
-
+              fileSystem.isDownloading(fileUrl, (downloading) => {
+                if (downloading) {
+                  sendDownloadingResponse(res, fileUrl);
+                } else {
+                  downloadFile(res, controller, fileUrl);
                 }
               });
             }
@@ -85,46 +61,7 @@ const FileRoute = function(app, headerDB, riseDisplayNetworkII, config, logger) 
             if (downloading) {
               sendDownloadingResponse(res, fileUrl);
             } else {
-              // Check if there's enough disk space.
-              fileSystem.getAvailableSpace(logger, (availableSpace) => {
-                // Download the file.
-                if (availableSpace > config.diskThreshold) {
-
-                  controller.on("headers-error", (err) => {
-                    logger.error("Could not save headers", err, fileUrl);
-                  });
-
-                  controller.on("downloaded", () => {
-                    logger.info("File downloaded", fileUrl);
-                  });
-
-                  controller.on("downloading", () => {
-                    sendDownloadingResponse(res, fileUrl);
-                  });
-
-                  controller.on("invalid-response", (statusCode) => {
-                    sendInvalidResponseResponse(res, fileUrl, statusCode);
-                  });
-
-                  controller.on("request-error", (err) => {
-                    logger.error(err, null, fileUrl);
-                    sendResponse(res, 504, "File's host server could not be reached", fileUrl);
-                  });
-
-                  controller.on("move-file-error", (err) => {
-                    logger.error(err, null, fileUrl);
-                  });
-
-                  controller.on("delete-file-error", (err) => {
-                    logger.error(err, null, fileUrl);
-                  });
-
-                  controller.downloadFile();
-                } else {
-                  logger.error("Insufficient disk space");
-                  sendResponse(res, 507, "Insufficient disk space");
-                }
-              });
+              downloadFile(res, controller, fileUrl);
             }
           });
         }
@@ -150,6 +87,49 @@ const FileRoute = function(app, headerDB, riseDisplayNetworkII, config, logger) 
     }
   }
 
+  function downloadFile(res, controller, fileUrl) {
+    // Check if there's enough disk space.
+    fileSystem.getAvailableSpace(logger, (availableSpace) => {
+      // Download the file.
+      if (availableSpace > config.diskThreshold) {
+
+        controller.on("headers-error", (err) => {
+          logger.error("Could not save headers", err, fileUrl);
+        });
+
+        controller.on("downloaded", () => {
+          logger.info("File downloaded", fileUrl);
+        });
+
+        controller.on("downloading", () => {
+          sendDownloadingResponse(res, fileUrl);
+        });
+
+        controller.on("invalid-response", (statusCode) => {
+          sendInvalidResponseResponse(res, fileUrl, statusCode);
+        });
+
+        controller.on("request-error", (err) => {
+          logger.error(err, null, fileUrl);
+          sendResponse(res, 504, "File's host server could not be reached", fileUrl);
+        });
+
+        controller.on("move-file-error", (err) => {
+          logger.error(err, null, fileUrl);
+        });
+
+        controller.on("delete-file-error", (err) => {
+          logger.error(err, null, fileUrl);
+        });
+
+        controller.downloadFile();
+      } else {
+        logger.error("Insufficient disk space");
+        sendResponse(res, 507, "Insufficient disk space");
+      }
+    });
+  }
+  
   function sendDownloadingResponse(res, fileUrl) {
     sendResponse(res, 202, "File is downloading", fileUrl);
     logger.info("File is downloading", fileUrl);
