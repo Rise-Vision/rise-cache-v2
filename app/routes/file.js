46c9a2bebd435162d787dff1e68976143caf750c
Tyler Johnson <mail@tyler-johnson.ca>
2017-08-31 11:30:18 -0400

    Fix encoding issues

--- app/routes/file.js
+++ app/routes/file.js
@@ -3,147 +3,152 @@
 const fileSystem = require("../helpers/file-system"),
   FileController = require("../controllers/file"),
   Data = require("../models/data"),
-  URL = require("url");
+  URL_QUERY = "?url=";
 
 const FileRoute = function(app, headerDB, riseDisplayNetworkII, config, logger) {
 
   app.get("/files", (req, res, next) => {
+    if (!req.url.includes(URL_QUERY)) {
+      res.statusCode = 400;
+      return next(new Error("Missing url parameter"));
+    }
 
-    if (req.query.url) {
-      // Parse the URL for encoding again only the path for the file
-      let fileUrl = URL.parse(req.query.url);
-      fileUrl = `${fileUrl.protocol}//${fileUrl.host}/${encodeURIComponent(fileUrl.path.substring(1) + ((fileUrl.hash)? fileUrl.hash : ""))}`;
+    let fileUrl = req.url.split(URL_QUERY)[1];
 
-      const header = new Data({}, headerDB),
-        controller = new FileController(fileUrl, header, riseDisplayNetworkII, logger);
+    if (fileUrl.startsWith("https%3A%2F%2F")) {
+      fileUrl = fileUrl.replace("https%3A%2F%2F");
+    }
 
-      controller.on("file-error", (err) => {
-        res.statusCode = 500;
-        next(err);
-      });
+    if (fileUrl.startsWith("http%3A%2F%2F")) {
+      fileUrl = fileUrl.replace("http%3A%2F%2F");
+    }
 
-      // Check if the file is cached.
-      fileSystem.isCached(fileUrl, (cached) => {
-        if (cached) {
-          // Get file from disk and stream to client.
-          controller.getHeaders((err, headers) => {
+    const header = new Data({}, headerDB),
+      controller = new FileController(fileUrl, header, riseDisplayNetworkII, logger);
 
-            if (err) {
-              logger.error(err, null, fileUrl);
+    controller.on("file-error", (err) => {
+      res.statusCode = 500;
+      next(err);
+    });
+
+    // Check if the file is cached.
+    fileSystem.isCached(fileUrl, (cached) => {
+      if (cached) {
+        // Get file from disk and stream to client.
+        controller.getHeaders((err, headers) => {
+
+          if (err) {
+            logger.error(err, null, fileUrl);
+          }
+          else {
+            if (!headers) {
+              logger.error("No headers available", null, fileUrl);
             }
-            else {
-              if (!headers) {
-                logger.error("No headers available", null, fileUrl);
-              }
-            }
+          }
 
-            getFromCache(req, res, controller, fileUrl, headers);
+          getFromCache(req, res, controller, fileUrl, headers);
 
-          });
+        });
 
-          // check if file is stale
-          controller.isStale(config.fileUpdateDuration, (err, stale) => {
+        // check if file is stale
+        controller.isStale(config.fileUpdateDuration, (err, stale) => {
 
-            if (err) {
-              logger.error(err, null, fileUrl);
-            }
+          if (err) {
+            logger.error(err, null, fileUrl);
+          }
 
-            if (stale) {
+          if (stale) {
 
-              // Check if the file is downloading.
-              fileSystem.isDownloading(fileUrl, (downloading) => {
-                if (!downloading) {
+            // Check if the file is downloading.
+            fileSystem.isDownloading(fileUrl, (downloading) => {
+              if (!downloading) {
 
-                  // get the appropriate header field for request
-                  controller.getUpdateHeaderField((err, field) => {
-                    if (err) { logger.error(err, null, fileUrl); }
+                // get the appropriate header field for request
+                controller.getUpdateHeaderField((err, field) => {
+                  if (err) { logger.error(err, null, fileUrl); }
 
-                    controller.on("headers-error", (err) => {
-                      logger.error("Could not save headers", err, fileUrl);
-                    });
-
-                    controller.on("downloaded", () => {
-                      logger.info("File downloaded", fileUrl);
-                    });
-
-                    controller.on("request-error", (err) => {
-                      logger.error(err, null, fileUrl);
-                    });
-
-                    controller.on("move-file-error", (err) => {
-                      logger.error(err, null, fileUrl);
-                    });
-
-                    // Make request to download file passing request header
-                    controller.downloadFile(field);
+                  controller.on("headers-error", (err) => {
+                    logger.error("Could not save headers", err, fileUrl);
                   });
 
-                }
-              });
-            }
-          });
+                  controller.on("downloaded", () => {
+                    logger.info("File downloaded", fileUrl);
+                  });
 
-        } else {
-          // Check if the file is downloading.
-          fileSystem.isDownloading(fileUrl, (downloading) => {
-            if (downloading) {
-              sendDownloadingResponse(res, fileUrl);
-            } else {
-              fileSystem.getAvailableSpace(logger, (spaceInDisk)=>{
-                // Check if there's enough disk space.
-                fileSystem.isThereAvailableSpace(logger, (isThereAvailableSpace) => {
-                  if (isThereAvailableSpace) {
-                    // Download the file.
+                  controller.on("request-error", (err) => {
+                    logger.error(err, null, fileUrl);
+                  });
 
-                    controller.on("headers-error", (err) => {
-                      logger.error("Could not save headers", err, fileUrl);
-                    });
+                  controller.on("move-file-error", (err) => {
+                    logger.error(err, null, fileUrl);
+                  });
 
-                    controller.on("downloaded", () => {
-                      logger.info("File downloaded", fileUrl);
-                    });
+                  // Make request to download file passing request header
+                  controller.downloadFile(field);
+                });
 
-                    controller.on("downloading", () => {
-                      sendDownloadingResponse(res, fileUrl);
-                    });
+              }
+            });
+          }
+        });
 
-                    controller.on("invalid-response", (statusCode) => {
-                      sendInvalidResponseResponse(res, fileUrl, statusCode);
-                    });
+      } else {
+        // Check if the file is downloading.
+        fileSystem.isDownloading(fileUrl, (downloading) => {
+          if (downloading) {
+            sendDownloadingResponse(res, fileUrl);
+          } else {
+            fileSystem.getAvailableSpace(logger, (spaceInDisk)=>{
+              // Check if there's enough disk space.
+              fileSystem.isThereAvailableSpace(logger, (isThereAvailableSpace) => {
+                if (isThereAvailableSpace) {
+                  // Download the file.
 
-                    controller.on("request-error", (err) => {
-                      logger.error(err, null, fileUrl);
-                      sendResponse(res, 504, "File's host server could not be reached", fileUrl);
-                    });
+                  controller.on("headers-error", (err) => {
+                    logger.error("Could not save headers", err, fileUrl);
+                  });
 
-                    controller.on("move-file-error", (err) => {
-                      logger.error(err, null, fileUrl);
-                    });
+                  controller.on("downloaded", () => {
+                    logger.info("File downloaded", fileUrl);
+                  });
 
-                    controller.on("delete-file-error", (err) => {
-                      logger.error(err, null, fileUrl);
-                    });
+                  controller.on("downloading", () => {
+                    sendDownloadingResponse(res, fileUrl);
+                  });
 
-                    controller.on("insufficient-disk-space", (fileSize) => {
-                      logger.error("Insufficient disk space", fileSize, fileUrl);
-                      sendResponse(res, 507, "Insufficient disk space");
-                    });
+                  controller.on("invalid-response", (statusCode) => {
+                    sendInvalidResponseResponse(res, fileUrl, statusCode);
+                  });
 
-                    controller.downloadFile();
-                  } else {
-                    logger.error("Insufficient disk space", null, fileUrl);
+                  controller.on("request-error", (err) => {
+                    logger.error(err, null, fileUrl);
+                    sendResponse(res, 504, "File's host server could not be reached", fileUrl);
+                  });
+
+                  controller.on("move-file-error", (err) => {
+                    logger.error(err, null, fileUrl);
+                  });
+
+                  controller.on("delete-file-error", (err) => {
+                    logger.error(err, null, fileUrl);
+                  });
+
+                  controller.on("insufficient-disk-space", (fileSize) => {
+                    logger.error("Insufficient disk space", fileSize, fileUrl);
                     sendResponse(res, 507, "Insufficient disk space");
-                  }
-                }, spaceInDisk);
-              });
-            }
-          });
-        }
-      });
-    } else {
-      res.statusCode = 400;
-      next(new Error("Missing url parameter"));
-    }
+                  });
+
+                  controller.downloadFile();
+                } else {
+                  logger.error("Insufficient disk space", null, fileUrl);
+                  sendResponse(res, 507, "Insufficient disk space");
+                }
+              }, spaceInDisk);
+            });
+          }
+        });
+      }
+    });
   });
 
   function getFromCache(req, res, controller, fileUrl, headers) {
